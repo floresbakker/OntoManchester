8c7d52097eb670cee55562cf55af3f58ada9b379
f.bakker@minfin.nl <f.bakker@minfin.nl>
2023-09-18 16:53:35 +0200

    WORK IN PROGRESS: improved manchestersyntax.ttl & python file

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# manchestersyntax
-A RDF-based vocabulary for the manchester syntax
+# Manchester Syntax
+
+A RDF-based vocabulary for Manchester Syntax.
+
+## Background
+
+Manchester Syntax is a human-readable syntax used to represent and work with knowledge expressed in the Web Ontology Language (OWL). OWL is a semantic web language designed for expressing ontologies, which are formal representations of knowledge about a specific domain. These ontologies are used to enable computers to understand and reason about the semantics of information on the web.
+
+Manchester Syntax provides a more user-friendly and compact way to write OWL expressions and axioms. It's particularly useful for ontology engineers and domain experts who want to create, publish and maintain OWL ontologies, as it makes the language more accessible.
+
+Transforming OWL to Manchester Syntax currently involves either manually translating one to the other, or using transformation functions in tools and libraries like Protégé and [OWL API](https://github.com/owlcs/owlapi). These functions are usually written in traditional programming languages.
+
+Unfortunately, there is currently no RDF-based vocabulary that can map OWL to Manchester Syntax automatically. Having a RDF-based vocabulary for Manchester Syntax would through its semantics add to the understandability, auditability and maintainability of the mapping. In addition, it offers the benefit to an ontology engineer or domain expert of actually working with RDF when modeling RDF, and not through some arbitrary programming language or black box software function. Last but not least, RDF is all about re-usability; having Manchester Syntax available as a set of data offering interoperability and semantics is in line with the goals of the Semantic Web and the knowledge graph technology underneath it. 
+
+## Ontology of the Manchester Syntax
+
+The ontology of the Manchester Syntax consists of shapes according to the SHACL-format. 
+
+It has NodeShapes for the following class constructs:
+
+1. A class definition (owl:Class);
+2. A restriction class with the property owl:allValuesFrom
+3. A restriction class with the property owl:someValuesFrom
+4. A restriction class with the property owl:hasValue
+5. A restriction class with the property owl:cardinality
+6. A restriction class with the property owl:maxCardinality
+7. A restriction class with the property owl:cardinality
+8. A restriction class with the property owl:minCardinality
+9. A subclass of some other class (rdfs:SubClassOf property)
+10. An equivalent class of some other class (owl:equivalentClass)
+11. A class that is the union of other classes (owl:unionOf)
+12. A class that is the intersection of other classes (owl:intersectionOf)
+13. A class that consists of the enumeration of individuals (owl:oneOf)
+
+Still to be done:
+
+14. owl:complementOf
+15. Object properties
+16. Datatype properties
+17. ..?
+
+### Example
+
+Work in progress
+
+# Tools and dependencies
+
+This repository comes with a, fairly primitive, Python-based tool to transform OWL into Manchester Syntax.
+
+1. manchestersyntax.py
+
+## manchestersyntax.py
+
+The tool manchestersyntax.py is used to read OWL ontologies and express them in Manchester Syntax.
+
+### How to use manchestersyntax.py
+
+A. Install all necessary libraries:
+
+	1. pip install os
+	1. pip install pyshacl
+	2. pip install rdflib
+
+B. Place one or more OWL-ontologies in the input folder in \manchestersyntax\Tools\Input. 
+
+C. Run the script in the command prompt by typing: 
+
+```
+python manchestersyntax.py
+```
+
+D. Go to the output folder in \manchestersyntax\Tools\Output and grab your Turtle-file(s) (*.ttl). 
--- Tools/ManchesterSyntax.py
+++ Tools/ManchesterSyntax.py
@@ -4,11 +4,24 @@ Created on Wed Jul  5 18:40:53 2023
 
 @author: Flores Bakker
 
+The Manchester Syntax Vocabulary models the Manchester Syntax in RDF. Manchester Syntax is a human-readable and compact syntax used to represent knowledge in the Web Ontology Language (OWL), which is a semantic web language based on RDF and used for defining and sharing ontologies. Manchester Syntax is designed to be human-readable and concise, making it easier for users to create and understand OWL ontologies. It provides an alternative to the more verbose syntax of OWL itself.
+
+With the script ManchesterSyntax.py one can transform OWL into Manchester Syntax.
+
+Usage: 
+
+1. Place an arbitrary ontology (as turtle file) in the input folder.
+2. In the command prompt, run 'python manchestersyntax.py'
+3. Go to the output folder and grab your enriched turtle file, now including Manchester Syntax labels and definitions.
+
 
 """
+import os
 import pyshacl
 import rdflib 
 
+# Set the path to the desired standard directory. 
+directory_path = "C:/Users/Administrator/Documents/Branches/"
 
 # Function to read a graph (as a string) from a file 
 def readGraphFromFile(file_path):
@@ -20,7 +33,7 @@ def readGraphFromFile(file_path):
 
 # Function to write a graph to a file
 def writeGraph(graph):
-    graph.serialize(destination="C:/Users/Administrator/Documents/Branches/ManchesterSyntax/manchesterDocument.ttl", format="turtle")
+    graph.serialize(destination="C:/Users/Administrator/Documents/Branches/manchestersyntax/Tools/Output/"+filename_stem+"-manchestersyntax.ttl", format="turtle")
 
 # Function to call the PyShacl engine so that a RDF model of an HTML document can be serialized to HTML-code.
 def iteratePyShacl(manchester_generator, serializable_graph):
@@ -41,24 +54,32 @@ def iteratePyShacl(manchester_generator, serializable_graph):
         writeGraph(serializable_graph)
              
 
-# Get the HTML vocabulary and place it in a string
-manchester_generator = readGraphFromFile("C:/Users/Administrator/Documents/Branches/ManchesterSyntax/manchestersyntax.ttl")
+# loop through any html files in the input directory
+for filename in os.listdir(directory_path+"manchestersyntax/Tools/Input"):
+    if filename.endswith(".ttl"):
+        file_path = os.path.join(directory_path+"manchestersyntax/Tools/Input", filename)
+        
+        # Establish the stem of the file name for reuse in newly created files
+        filename_stem = os.path.splitext(filename)[0]
+        
 
-# Get the RDF-model of some HTML document and place it in a string. Here you can input your own RDF-model of some HTML-document.
-ontology_graph = readGraphFromFile("C:/Users/Administrator/Documents/Branches/HTML voc/HTML vocabulary.ttl")   
+# Get the manchester syntax vocabulary and place it in a string
+manchester_generator = readGraphFromFile("C:/Users/Administrator/Documents/Branches/manchestersyntax/Specification/manchestersyntax.ttl")
 
-# Join the HTML vocabulary and the RDF-model of some HTML document into a string
+# Get some ontology to be transformed from OWL to Manchester Syntax. The ontology needs to be placed in the input directory.
+ontology_graph = readGraphFromFile(file_path)   
+
+# Join the manchester syntax and the ontology to be transformed into one big string.
 serializable_graph_string = ontology_graph
 
-# Create a graph of the string containing the HTML vocabulary and the RDF-model of some HTML document
+# Create a graph of the string consisting of the manchester syntax and the ontology to be transformed 
 serializable_graph = rdflib.Graph().parse(data=serializable_graph_string , format="ttl")
 
 # Inform user
-print ('Instantiating manchester document...')
+print ('Creating Manchester Syntax labels and definitions...')
 
 # Call the shacl engine with the HTML vocabulary and the document to be serialized
 iteratePyShacl(manchester_generator, serializable_graph)
 
-
-
-print ('manchester document instantiated.')
+# Inform user
+print ('Manchester Syntax labels and definitions created and added to the ontology.')
